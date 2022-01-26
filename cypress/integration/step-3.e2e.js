d52e1d398958b10192137bef538a17c9e35fb13f
Jeff Chew <jeff.chew@gmail.com>
2022-01-26 15:07:33 -0500

    test(tutorial): fix cardinality test

--- cypress/integration/step-3.e2e.js
+++ cypress/integration/step-3.e2e.js
@@ -14,22 +14,29 @@ describe('step-3', () => {
   });
 
   it('should load first Content block with Content group and cards', () => {
-    cy.get('dds-content-section > dds-content-block > dds-content-group > dds-card-group')
+    cy.get(
+      'dds-content-section > dds-content-block > dds-content-group > dds-card-group'
+    )
       .first()
       .find('dds-card-group-item')
       // 5 + 1 empty card
-      .should('have.length', 5);
+      .should('have.length', 6);
 
     cy.screenshot();
   });
 
   it('should load second Content block with Card in card and Content group ', () => {
-    cy.get('dds-content-section > dds-content-block > dds-video-cta-container > dds-card-in-card')
-      .should('be.visible');
+    cy.get(
+      'dds-content-section > dds-content-block > dds-video-cta-container > dds-card-in-card'
+    ).should('be.visible');
 
-    cy.get('dds-content-section > dds-content-block > dds-card-link-cta').should('be.visible');
+    cy.get(
+      'dds-content-section > dds-content-block > dds-card-link-cta'
+    ).should('be.visible');
 
-    cy.get('dds-content-section > dds-content-block > dds-content-group > dds-card-group')
+    cy.get(
+      'dds-content-section > dds-content-block > dds-content-group > dds-card-group'
+    )
       .eq(1)
       .find('dds-card-group-item')
       .should('have.length', 3);
@@ -46,11 +53,10 @@ describe('step-3', () => {
       .find('dds-content-item')
       .should('have.length', 3);
 
-
-    cy.get('dds-content-section > dds-content-block > dds-content-group > dds-card-link-cta')
-      .should('be.visible');
+    cy.get(
+      'dds-content-section > dds-content-block > dds-content-group > dds-card-link-cta'
+    ).should('be.visible');
 
     cy.screenshot();
   });
 });
-
