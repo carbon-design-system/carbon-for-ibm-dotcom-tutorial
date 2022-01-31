d639e90c256ae97b34a81327c7095fceaa59bfaf
Nisha Nair <nisha.nair.k@gmail.com>
2022-01-31 09:00:10 -0800

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,2 @@
-import '../scss/index.scss';
-import '../scss/app.scss';
-import '@carbon/ibmdotcom-web-components/es/components/button-group/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
+import '../scss/app.scss';
