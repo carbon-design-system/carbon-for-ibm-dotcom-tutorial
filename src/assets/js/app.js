d4ddd92054c365c7ecb41fd5a4e9eb900db3d296
Matej Komlóši <matej.komlosi@gmail.com>
2022-02-01 12:02:55 +0100

    feat(tutorial): complete step 1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,3 @@
-import '../scss/index.scss';
 import '../scss/app.scss';
 import '@carbon/ibmdotcom-web-components/es/components/button-group/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
