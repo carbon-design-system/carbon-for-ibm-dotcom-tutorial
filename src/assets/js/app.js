fc10cf2dfcfc3589ff7cfe74b65371a66ab11ea7
Tadas <tadas.ganitauskas@ibm.com>
2022-02-17 18:06:40 +0200

    feat(tutorial): complete step 1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,5 @@
+import '@carbon/ibmdotcom-web-components/es/components/button-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
+
 import '../scss/index.scss';
+import '../scss/app.scss';
