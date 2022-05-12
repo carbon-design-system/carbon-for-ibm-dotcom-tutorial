ee7f95e3e53b5ca706526cb01a91f77e574e7111
Nils von Rymon-Lipinski <nils.vonrymon-lipinski@aperto.com>
2022-05-12 21:07:35 +0200

    feat(tutorial): complete step 2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,5 @@
 import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
 import '../scss/app.scss';
+import '@carbon/ibmdotcom-web-components/es/components/leadspace-with-search/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
