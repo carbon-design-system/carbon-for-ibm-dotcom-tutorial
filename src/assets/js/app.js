828e783f34a2641e97b12c4c2442e27995c10786
Nils von Rymon-Lipinski <nils.vonrymon-lipinski@aperto.com>
2022-05-17 20:45:33 +0200

    feat(tutorial): complete step 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,15 @@
+import '../scss/app.scss';
 import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/leadspace-with-search/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta-section/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/link-list/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/content-section/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/cta-section/index.js';
-
-import '../scss/app.scss';
+import '@carbon/ibmdotcom-web-components/es/components/content-block/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/image/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-in-card/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/video-cta-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/index.js';
