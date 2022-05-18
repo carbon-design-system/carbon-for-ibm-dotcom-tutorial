563cebd663cce164f2f8235797c0a246bd96e89b
Marco Cortez <macortezva@gmail.com>
2022-05-18 16:36:49 -0500

    feat(tutorial): complete step 4

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,7 +16,12 @@ import '@carbon/ibmdotcom-web-components/es/components/image-with-caption/index.
 import '@carbon/ibmdotcom-web-components/es/components/cta/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta/video-cta-container.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta/link-list-item-card-cta.js';
-import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/card-in-card/index.js';
+
+import 'carbon-web-components/es/components/tag/tag';
+import 'carbon-web-components/es/components/tabs/index';
+import 'carbon-web-components/es/components/data-table/index';
+import 'carbon-web-components/es/components/radio-button/index';
+import 'carbon-web-components/es/components/structured-list/index';
 
 import '../scss/app.scss';
