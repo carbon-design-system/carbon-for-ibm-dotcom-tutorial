ae75082d6dd1696ce9ca91c10bc821f53a40099b
Lizbeth Linares <lizbeth.linares@ibm.com>
2022-05-30 15:20:16 -0500

    feat(tutorial): complete step 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,4 +6,14 @@ import '@carbon/ibmdotcom-web-components/es/components/link-list/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/content-section/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta-section/index.js';
 
+import '@carbon/ibmdotcom-web-components/es/components/content-section/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-block/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/image/index.js';
+
+import '@carbon/ibmdotcom-web-components/es/components/card-in-card/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/video-cta-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/index.js';
+
 import '../scss/app.scss';
