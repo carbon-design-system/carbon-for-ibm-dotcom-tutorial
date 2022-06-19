c0d2d92cf19010a7bf75fec88f65b596024a2b98
nicollyrocha <nicollyrcs@gmail.com>
2022-06-19 15:02:31 -0300

    feat(tutorial): complete step 1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,3 @@
-import '../scss/index.scss';
+import '@carbon/ibmdotcom-web-components/es/components/button-group/index.js';
+import '../scss/app.scss';
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
