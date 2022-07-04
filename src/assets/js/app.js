51f980736ab626a589bce0e4c6690fbf12cc5785
Mathis.Moder <mathis.moder@aperto.com>
2022-07-04 11:12:12 +0200

    feat(tutorial): complete step 1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,5 @@
 import '../scss/index.scss';
+import '../scss/app.scss';
+
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/button-group/index.js';
