0f1847a59cd09a74e87f286c85ca1ef885f1a3ab
Björn Strobach <bjoern.strobach@ibm.com>
2022-08-17 14:29:14 +0200

    feat(tutorial): complete step 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,18 @@
-import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-in-card/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/leadspace-with-search/index.js';
-import '@carbon/ibmdotcom-web-components/es/components/link-list/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-block/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-group/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/content-section/index.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta-section/index.js';
-
+import '@carbon/ibmdotcom-web-components/es/components/cta/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/link-list-item-card-cta.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/video-cta-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/image-with-caption/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/image/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/leadspace-with-search/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/link-list/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
 import '../scss/app.scss';
