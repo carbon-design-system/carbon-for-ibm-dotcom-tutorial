c4d292d76840393d4715a1ec8bce66a2cbe457a1
luisfer <luisz15@hotmail.com>
2022-12-12 12:37:32 -0600

    feat(tutorial): complete step 2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,7 @@
 import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index.js';
 import '../scss/app.scss';
+import '@carbon/ibmdotcom-web-components/es/components/leadspace-with-search/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/table-of-contents/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-section-offset/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta-section/index.js';
+import '@carbon/ibmdotcom-web-components/es/components/link-list/index.js';
